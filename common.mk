52637bd46c9e3a695c997cc46e51a11374c08611
LuK1337 <priv.luk@gmail.com>
2021-05-24 18:23:42 +0200

    sdm845-common: Stub out vendor TelephonyResCommon overlay
    We are providing our own telephony overlays in place.
    
    Change-Id: If359af4060b5c653899912c2916ef0c93932bc0b

--- common.mk
+++ common.mk
@@ -33,6 +33,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-candy
 
+PRODUCT_PACKAGES += \
+    TelephonyResCommon
+
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
 # VNDK
